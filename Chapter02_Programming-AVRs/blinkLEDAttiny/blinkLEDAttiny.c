91b2e6747ca63e7b2d77d26686b029f7bf064eb0
John (Jack) Brown <jack@brownjohnf.com>
2014-04-04 09:49:54 -0700

    add port of blink to attiny85

---
+++ Chapter02_Programming-AVRs/blinkLEDAttiny/blinkLEDAttiny.c
@@ -0,0 +1,69 @@
+                                                       /* Blinker Demo */
+
+// ------- Preamble -------- //
+#include <avr/io.h>                        /* Defines pins, ports, etc */
+#include <util/delay.h>                     /* Functions to waste time */
+#include <avr/interrupt.h>
+#include <avr/power.h>
+
+// how long to give people before cutting the light (in ms)
+#define DELAY 30000
+
+// how many times we saw motion in our delay cycle
+volatile uint16_t motionCount;
+
+void initPinChangeInterrupt(void) {
+  // enable pin-change interrupts for bank b
+  GIFR |= (1 << PCIF);
+
+  // enable interrupt on pin PB4 (our motion sensor)
+  GIMSK |= (1 << PB4);
+}
+
+ISR(PCINT0_vect) {
+  motionCount++;
+  _delay_us(100);
+}
+
+int main(void) {
+
+  // -------- Inits --------- //
+
+  /* Data Direction Register B: writing a one to the bit enables output. */
+  DDRB |= (1 << PB0);
+
+  // turn on the relay
+  PORTB |= (1 << PB0);
+
+  // write a 0 to confirm input is enabled
+  //DDRB |= (0 << PB4);
+
+  // enable the pull-up resistor on PB4
+  //PORTB |= (1 << PB4);
+
+  initPinChangeInterrupt();
+
+  // ------ Event loop ------ //
+  while (1) {
+    motionCount = 0;
+    sei();
+    _delay_ms(DELAY);
+    cli();
+
+    if (motionCount > 0) {
+      PORTB &= ~(1 << PB0);
+    } else {
+      PORTB |= (1 << PB0);
+    }
+
+    /* look for button press */
+    // if ((PINB & (1 << PB4)) == 0) {
+
+    //   PORTB &= ~(1 << PB0);                                   /* pressed */
+    // }
+    // else {                                              /* not pressed */
+    //   PORTB |= (1 << PB0);
+    // }
+  }                                                  /* End event loop */
+  return (0);                            /* This line is never reached */
+}
